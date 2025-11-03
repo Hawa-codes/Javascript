458adffc1e7ccfff6bb8d0c1c9b015622828f55a
Hawa-codes <adediranhawa@gmail.com>
2025-11-03 12:57:58 +0100

    Blackjack Game Complete

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,23 +1,50 @@
-let firstCard = 10;
-let secondCard = 4;
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+let player = {
+    name: "Per",
+    chips: 145
+}
+
+let cards = []
+let sum = 0
 let hasBlackJack = false;
-
-let isAlive = true;
+let isAlive = false;
 let message = ""
-
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector('#sum-el')
 let cardEl = document.querySelector('#card-el')
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = playerName + ": $" + playerChips
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard() 
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
+    cardEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 21) {
-    message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "Wohoo! You've got Blackjack"
         hasBlackJack = true;
@@ -26,47 +53,16 @@ function renderGame() {
         isAlive = false;
     }
     messageEl.textContent = message
-    sumEl.textContent = "Sum: " + sum;
-    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    
 }
 
 function newCard() {
-    console.log("Draw a new card from the deck")
-    let card = 6;
-    sum += card;
-
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+       let card = getRandomCard();
+        sum += card;
+        cards.push(card)
+        console.log(cards)
+        renderGame() 
+    }
 }
 
-// CASH OUT     
-console.log(hasBlackJack)
-
-// let age = 22;
-
-// if (age < 21) {
-//     console.log("You can not enter the nightclub")
-// } else {
-//     console.log("Welcome!")
-// }
-
-// let age = 100;
-
-// if (age < 100) {
-//     console.log("Not Eligible")
-// } else if (age === 100) {
-//     console.log("Here is your birthday card from the king")
-// } else {
-//     console.log("Not eligible, you've already gotten one")
-// }
-
-// let hasDiscountCode = true;
-
-// function processOrder() {
-//     if (hasDiscountCode) {
-//         console.log("Discount applied to food order")
-//     } else {
-//         console.log("No disccount applied")
-//     }
-// } 
