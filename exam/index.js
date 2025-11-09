96fd3c247107d68950428169fca3c8c252c692f6
Hawa-codes <adediranhawa@gmail.com>
2025-11-09 21:58:27 +0100

    Javascript

--- exam/index.js
+++ exam/index.js
@@ -1,7 +1,9 @@
-let boxEl = document.getElementById("box")
+let btns = []
 
-boxEl.addEventListener("click", function() {
-    console.log("I want to open the box")
-})
+const container = document.getElementById("container")
 
-w
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy() {
+    container.innerHTML = "<p>Thanks for buying</p>"
+}
