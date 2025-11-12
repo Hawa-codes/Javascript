26fc8b662fa9b8efd9b363b50e37d8b809076b4b
Hawa-codes <adediranhawa@gmail.com>
2025-11-12 21:53:58 +0100

    javascript

--- exam/index.js
+++ exam/index.js
@@ -4,6 +4,4 @@ const container = document.getElementById("container")
 
 container.innerHTML = "<button onclick='buy()'>Buy!</button>"
 
-function buy() {
-    container.innerHTML = "<p>Thanks for buying</p>"
-}
+
