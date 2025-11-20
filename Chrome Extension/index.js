c392cfd208b48513e27966ebba682da5e7b8b29a
Hawa-codes <adediranhawa@gmail.com>
2025-11-20 13:57:45 +0100

    Chrome Extension Complete

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -2,23 +2,48 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const saveBtn = document.getElementById("save-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs =[ 
+    {url:"https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                  ${myLeads[i]}
+                </a>
+            </li>
+        ` 
+    }
+    ulEl.innerHTML = listItems
+}
+
+saveBtn.addEventListener("click", function() {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads ))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear()
+    myLeads =[]
+    render(myLeads)
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                 ${myLeads[i]}
-                <a/>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
