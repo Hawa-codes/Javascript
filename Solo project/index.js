9b50ccbf23fa74e35edf792c1e23cff1d79f1419
Hawa-codes <adediranhawa@gmail.com>
2025-11-20 21:11:59 +0100

    Solo project complete

--- Solo project/index.js
+++ Solo project/index.js
@@ -1,6 +1,19 @@
 let input = document.getElementById("input-btn")
 let btn = document.getElementById("btn")
+let answer1 = document.getElementById("answer1")
+let answer2 = document.getElementById("answer2")
+let answer3 = document.getElementById("answer3")
 
-inputBtn.addEventListener("click", function() {
-    
+btn.addEventListener("click", function() {
+    const value = Number(input.value)
+
+    answer1.textContent = `${value} meters = ${(value * 3.281).toFixed(3)} feet | ${value} feet = ${(value / 3.281).toFixed(3)} meters`;
+    answer2.textContent = `${value} liters = ${(value * 0.264).toFixed(3)} gallons | ${value} gallons = ${(value / 0.264).toFixed(3)} liters`;
+    answer3.textContent = `${value} kilos = ${(value * 2.204).toFixed(3)} pounds | ${value} pounds = ${(value / 2.204).toFixed(3)} kilos`;
 })
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallons
+1 kilogram = 2.204 pounds 
+*/
