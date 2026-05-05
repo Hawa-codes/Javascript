c05f27d7cb54ab91e158984fe3771ff6f1eb865c
Hawa-codes <adediranhawa@gmail.com>
2026-05-05 18:02:13 +0100

    Done

--- Todo-app/index.js
+++ Todo-app/index.js
@@ -1,3 +1,34 @@
-const inputEl = document.getElementById("input");
-const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const button = document.getElementById("btn");
 const result = document.getElementById("result");
+
+button.addEventListener("click", function () {
+    const task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.textContent = task;
+
+    span.addEventListener("click", function () {
+    span.classList.toggle("done");
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", function () {
+        li.remove();
+    });
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    result.appendChild(li);
+
+    input.value = "";
+});
